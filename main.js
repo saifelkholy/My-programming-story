29dd00a76511393c77ad2d289a2a7a09d9130956
saifelkholy <140922311+saifelkholy@users.noreply.github.com>
2023-12-22 22:13:29 +0200

    main.js
    JavaScript file added

---
+++ main.js
@@ -0,0 +1,14 @@
+
+        
+    let x = document.getElementById("para");
+    
+     
+    
+    if(document.getElementById("btn").addEventListener("mouse-left", function(){
+        x.style.color="white"
+    })){
+
+    }
+    else{
+x.style.color="aqua"
+    }
