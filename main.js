11ae49f63e89e00c828f28caf1663495dd7c93ef
saifelkholy <140922311+saifelkholy@users.noreply.github.com>
2023-12-23 14:54:55 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+
+    let x = document.getElementById("para")
+    let y =document.getElementById("btn_Chapter_I_go_back")
+    let z =document.getElementById("Chapter_II")
+    let m =document.getElementById("btn_Chapter_II") 
+    let w =document.getElementById("Chapter_III")
+    let o =document.getElementById("btn_Chapter_III_go_back") 
+    let a =document.getElementById("Chapter_IV_main")
+    let i =document.getElementById("btn_Chapter_III")
+    let r =document.getElementById("mode")
+    let p =document.getElementById("body_index")
+    let l =document.getElementById("Main_part")
+
+
+    if(document.getElementById("btn_Chapter_I").addEventListener("click", function(){
+        x.style.display ="none"
+        y.style.display="unset"
+        z.style.display="unset"
+    }) ){
+
+    }
+    if(document.getElementById("btn_Chapter_I_go_back").addEventListener("click", function(){
+    x.style.display="unset"
+    y.style.display="none"
+    z.style.display="none"
+
+
+    }) ){
+
+    }
+    if(document.getElementById("btn_Chapter_II").addEventListener("click", function(){
+    w.style.display="unset"
+    z.style.display="none"
+    
+    
+        }) ){
+    
+        }
+
+        if(document.getElementById("btn_Chapter_III_go_back").addEventListener("click", function(){
+        z.style.display="unset"
+        w.style.display="none"    
+            
+                }) ){
+            
+                }
+
+
+
+    
+
+
+      
+
+
+      
+
